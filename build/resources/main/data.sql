dd4c97661359b4eabdfa983c6fc7f68a5f4c0601
Shane Lenagh <shanelenagh@gmail.com>
2018-04-10 09:28:50 -0500

    Removing erroneously checked in build dir

--- build/resources/main/data.sql
+++
@@ -1,9 +0,0 @@
---create table PARAMOTOR(
---	id bigint auto_increment,
---	name varchar(100),
---	engineName varchar(100),
---	engineSizeCC NUMBER(10,2),
---	propSizeInches integer
---);
-insert into Paramotor(pmtr_name, engn_name, engn_size_cc, prop_size_inch)
-VALUES('Shane''s Paramotor', 'Polini Thor', 130.0, 125);
